b89863f49a8b9976f43c136cface5250a4d0cd57
Brent Gulanowski <brent@shopify.com>
2014-11-19 06:16:18 -0500

    Add iOS target.

---
+++ LichenMath-iOS/LichenMath-iOS.h
@@ -0,0 +1,19 @@
+//
+//  LichenMath-iOS.h
+//  LichenMath-iOS
+//
+//  Created by Brent Gulanowski on 2014-11-15.
+//  Copyright (c) 2014 Lichen Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LichenMath-iOS.
+FOUNDATION_EXPORT double LichenMath_iOSVersionNumber;
+
+//! Project version string for LichenMath-iOS.
+FOUNDATION_EXPORT const unsigned char LichenMath_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LichenMath_iOS/PublicHeader.h>
+
+
