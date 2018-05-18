e2232c15a67b672c8619f21c29c58e9e4cb98369
Brent Gulanowski <bgulanowski@gmail.com>
2018-05-18 10:03:28 -0400

    Add missing imports from umbrella.

--- LichenMath-iOS/LichenMath-iOS.h
+++ LichenMath-iOS/LichenMath-iOS.h
@@ -14,6 +14,9 @@ FOUNDATION_EXPORT double LichenMath_iOSVersionNumber;
 //! Project version string for LichenMath-iOS.
 FOUNDATION_EXPORT const unsigned char LichenMath_iOSVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <LichenMath_iOS/PublicHeader.h>
-
+#import <LichenMath/LIMatrix.h>
+#import <LichenMath/LIPoint.h>
+#import <LichenMath/LIRange.h>
+#import <LichenMath/LIRegion.h>
+#import <LichenMath/LIVector.h>
 
